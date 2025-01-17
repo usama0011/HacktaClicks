af3b707d451735e4bdec71dc5885cc67d24950fb
unknown <za5232208@gmail.com>
2025-01-17 12:36:35 +0500

    UploadDotEnv

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 //db
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://za5232208:za5232208@cluster0.ya4tk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);

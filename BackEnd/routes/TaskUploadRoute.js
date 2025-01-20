f3adb7a9c9a44ed8caae42cf6011c1e13dc53477
unknown <za5232208@gmail.com>
2025-01-20 16:41:08 +0500

    NOWLETSEE

--- BackEnd/routes/TaskUploadRoute.js
+++ BackEnd/routes/TaskUploadRoute.js
@@ -10,14 +10,6 @@ cloudinary.config({
 });
 const router = express.Router();
 
-// Middleware to allow Cloudinary headers if needed
-router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://res.cloudinary.com");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 // Get all task uploads
 router.get("/", async (req, res) => {
   try {

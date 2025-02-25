7f8cd39793c71d63cd61d7aa0026a1df382d8edd
unknown <za5232208@gmail.com>
2025-02-25 10:13:04 +0500

    BASeURLChage

--- FrontEnd/src/components/BaseURL.js
+++ FrontEnd/src/components/BaseURL.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //http://localhost:5000/api
 //https://hackta-clicks-backend.vercel.app/api
 const baseURL = "https://hackta-clicks-backend.vercel.app/api"; // Replace with your API base URL
-
+//hello
 const axiosInstance = axios.create({
   baseURL,
   headers: {
